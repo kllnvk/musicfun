3229dc9ac8fe1cd8dcde266d70b0f9e0cef2917a
Kiryl Novik <kllnvk13@gmail.com>
2026-06-23 18:09:50 +0300

    Initialize project

---
+++ src/app/ui/App.tsx
@@ -0,0 +1,12 @@
+import {Routing} from "@/common/routing/Routing";
+
+function App() {
+
+    return (
+        <>
+            <Routing/>
+        </>
+    )
+}
+
+export default App
---
+++ src/common/routing/Routing.tsx
@@ -0,0 +1,24 @@
+import {MainPage} from "@/app/ui/MainPage/MainPage";
+import {PageNotFound} from "@/common/components/PageNotFound/PageNotFound";
+import {ProfilePage} from "@/features/auth/ui/ProfilePage/ProfilePage";
+import {PlaylistsPage} from "@/features/playlists/ui/PlaylistsPage";
+import {TracksPage} from "@/features/tracks/ui/TracksPage";
+import {Route, Routes} from "react-router";
+
+export const Path = {
+    Main: '/',
+    Playlists: '/playlists',
+    Tracks: '/tracks',
+    Profile: '/profile',
+    NotFound: '*',
+} as const
+
+export const Routing = () => (
+    <Routes>
+        <Route path={Path.Main} element={<MainPage />} />
+        <Route path={Path.Playlists} element={<PlaylistsPage />} />
+        <Route path={Path.Tracks} element={<TracksPage />} />
+        <Route path={Path.Profile} element={<ProfilePage />} />
+        <Route path={Path.NotFound} element={<PageNotFound />} />
+    </Routes>
+)
